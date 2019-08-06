d6a0eeccd39cc5f0dd2041a1b79fe289b956c6d2
Raul Fernandes <raulkfernandes@gmail.com>
2019-08-06 15:47:38 -0300

    Alterando extensão para AppCompatActivity e título do AppBar

--- app/src/main/java/br/com/alura/agenda/MainActivity.java
+++ app/src/main/java/br/com/alura/agenda/MainActivity.java
@@ -1,22 +1,23 @@
 package br.com.alura.agenda;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Lista de Alunos");
 
         List<String> alunos = new ArrayList<>(Arrays.asList("Eu", "Tu", "Ele"));
         ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
