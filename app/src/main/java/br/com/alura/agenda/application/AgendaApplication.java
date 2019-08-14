7f3a734836836afc79b36b19c59f9056731ff28c
Raul Fernandes <raulkfernandes@gmail.com>
2019-08-14 02:22:11 -0300

    Implementando extensão de Application para comportamentos iniciais do app

---
+++ app/src/main/java/br/com/alura/agenda/application/AgendaApplication.java
@@ -0,0 +1,22 @@
+package br.com.alura.agenda.application;
+
+import android.app.Application;
+
+import br.com.alura.agenda.dao.AlunoDAO;
+import br.com.alura.agenda.model.Aluno;
+
+public class AgendaApplication extends Application {
+
+    AlunoDAO dao = new AlunoDAO();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        inicializaAlunosDeTeste();
+    }
+
+    private void inicializaAlunosDeTeste() {
+        dao.salvaAluno(new Aluno("Raul Fernandes", "998889328", "raulfelipe2@gmail.com"));
+        dao.salvaAluno(new Aluno("Ana Maria", "996685328", "raulkfernandes@gmail.com"));
+    }
+}
