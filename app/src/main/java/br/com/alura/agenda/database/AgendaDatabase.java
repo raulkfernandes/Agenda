070facafaa8b040b7b9aaec602bb39fa5add04e9
Raul Fernandes <raulkfernandes@gmail.com>
2020-02-14 16:17:24 -0300

    Adicionando migration com o novo campo ao database

--- app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
+++ app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
@@ -2,9 +2,12 @@ package br.com.alura.agenda.database;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import br.com.alura.agenda.database.dao.RoomAlunoDAO;
 import br.com.alura.agenda.model.Aluno;
@@ -18,12 +21,19 @@ public abstract class AgendaDatabase extends RoomDatabase {
     public abstract RoomAlunoDAO getRoomAlunoDAO();
 
     public static AgendaDatabase getInstance(Context mContext) {
-        if(instance == null) {
+        if (instance == null) {
             instance = Room
                     .databaseBuilder(mContext, AgendaDatabase.class, DATABASE_NAME)
                     .allowMainThreadQueries()
+                    //.fallbackToDestructiveMigration()
+                    .addMigrations(new Migration(1, 2) {
+                        @Override
+                        public void migrate(@NonNull SupportSQLiteDatabase database) {
+                            database.execSQL("ALTER TABLE aluno ADD COLUMN sobrenome TEXT");
+                        }
+                    })
                     .build();
         }
-            return instance;
+        return instance;
     }
 }
