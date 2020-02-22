4ff086a499f4c730ace8fc69c2c04e57d617aed1
Raul Fernandes <raulkfernandes@gmail.com>
2020-02-21 23:37:09 -0300

    Refatorando Query de busca no TelefoneDAO

--- app/src/main/java/br/com/alura/agenda/database/dao/RoomTelefoneDAO.java
+++ app/src/main/java/br/com/alura/agenda/database/dao/RoomTelefoneDAO.java
@@ -7,10 +7,12 @@ import br.com.alura.agenda.model.Telefone;
 
 @Dao
 public interface RoomTelefoneDAO {
-    @Query("SELECT t.* FROM " +
-            "Telefone t JOIN Aluno a " +
-            "ON t.alunoID = a.id " +
-            "WHERE t.alunoId = :alunoId " +
-            "LIMIT 1")
+    
+//    @Query("SELECT t.* FROM " +
+//            "Telefone t JOIN Aluno a " +
+//            "ON t.alunoID = a.id " +
+//            "WHERE t.alunoId = :alunoId " +
+//            "LIMIT 1")
+    @Query("SELECT * FROM Telefone WHERE alunoId = :alunoId LIMIT 1")
     Telefone buscaPrimeiroTelefone(int alunoId);
 }
